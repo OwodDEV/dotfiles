323a9ba08ab42dd04d348811a5bbcac76e80483a
Yan <y.khamula@icloud.com>
2024-10-06 10:07:01 +0000

    added keymaps for gp

--- nvim/lua/plugins/gp.lua
+++ nvim/lua/plugins/gp.lua
@@ -4,6 +4,9 @@ function M.setup()
 	require("gp").setup({
 
 		chat_shortcut_respond = { modes = { "n" }, shortcut = "<CR>" },
+		chat_shortcut_delete = { modes = { "n", "i", "v", "x" }, shortcut = "<leader>ad" },
+		chat_shortcut_stop = { modes = { "n", "i", "v", "x" }, shortcut = "<leader>as" },
+		chat_shortcut_new = { modes = { "n", "i", "v", "x" }, shortcut = "<leader>an" },
 
 		chat_user_prefix = "## PROMPT",
 		chat_assistant_prefix = { "### AI" },	
