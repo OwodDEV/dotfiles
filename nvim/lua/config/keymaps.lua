d64472a89b4fd97dbe24c5524a75f225c93045a9
Yan <y.khamula@icloud.com>
2025-01-18 10:35:06 +0000

    added keymaps for telescope

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -16,7 +16,9 @@ local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fe', builtin.diagnostics, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+vim.api.nvim_set_keymap('n', '<leader>fc', '<Cmd>:Telescope treesitter<CR>', { noremap = true, silent = true })
 
 -- lsp
 vim.api.nvim_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition({timeout = 1000})<CR>', { noremap = true, silent = true })
